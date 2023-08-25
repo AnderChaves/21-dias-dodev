9e0080057a7779ebe5f2d985e1a7203961029dd4
Anderson Chaves <andersonchvsilva@gmail.com>
2023-08-25 01:18:44 -0300

    trabalhando com variáveis e obtendo dados do usuário

--- Dia 3/dia3.js
+++ Dia 3/dia3.js
@@ -1,6 +1,17 @@
 //var let const
 
-let nome = 'Anderson'
-console.log = (nome)
-const idade = '22'
-console.log = (idade)
+let nomeUsuario = prompt('Digite o seu nome.')
+let idadeUsuario = Number(prompt('Digite a sua idade.'));
+let alturaUsuario = Number(prompt('Digite a sua altua.'));
+let pesoUsuario = Number(prompt('Digite o seu peso.'));
+let anoNascimento = 2023 - idadeUsuario;
+let imcUsuario = pesoUsuario/(alturaUsuario*alturaUsuario);
+console.log ("Olá " + nomeUsuario + ", você tem " + idadeUsuario + " anos, nasceu em " + anoNascimento + ", tem " + alturaUsuario + " de altura, pesa " + pesoUsuario+ " kg seu IMC é " + imcUsuario + "Kg/m2");
+
+
+
+
+
+
+
+
